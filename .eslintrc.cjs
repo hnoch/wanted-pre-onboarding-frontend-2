9c8f959d723f0ad914fda0b36696967ee951df5d
hnoch <hnochx@gmail.com>
2023-08-30 04:02:12 +0900

    🧹 chore: eslint & prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,23 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "no-var": "error", // var 금지
+    "no-multiple-empty-lines": "error", // 여러 줄 공백 금지
+    "no-console": ["error", { allow: ["warn", "error", "info"] }], // console.log() 금지
+    eqeqeq: "error", // 일치 연산자 사용 필수
+    "dot-notation": "error", // 가능하다면 dot notation 사용
+    "no-unused-vars": "error", // 사용하지 않는 변수 금지
   },
-}
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120, // 줄 바꿈 폭 길이
+  bracketSpacing: true, // 객체 리터럴 괄호 공백 여부
+  jsxSingleQuote: false, // jsx 홑따옴표 여부
+  singleQuote: false, // 홑따옴표 여부
+  trailingComma: "all", // 여러 줄의 경우 후행 콤마방식
+  arrowParens: "avoid", // 화살표 함수 사용 방식
+  semi: true, // 세미콜론 사용 여부
+  tabWidth: 2, // 탭 너비 공백 갯수
+};
